792fe541b30f50f789a4dcb4d7a14f86a4184aa1
shelldan <shelldanlee@gmail.com>
2023-01-17 21:39:20 -0600

    update on LoginForms.js and SignupForm.js

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,16 +2,18 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-//import { loginUser } from '../utils/API';
+// import useMutation and LOGIN_USER
 import { useMutation } from "@apollo/client"
 import { LOGIN_USER } from '../utils/mutations';
-import Auth from '../utils/auth';
 
+import Auth from '../utils/auth';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+
+  // declaring loginUser with useMutation
   const [loginUser, { error }] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
@@ -29,23 +31,12 @@ const LoginForm = () => {
       event.stopPropagation();
     }
 
+    // use loginUser function 
     try {
-      
       const { data } = await loginUser({
-        variables: {userFormData}
+        variables: { ...userFormData }
       })
 
-      /*
-      const response = await loginUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-      
-
-      const { token, user } = await response.json();
-      console.log(user);
-       */
       Auth.login(data.loginUser.token);
      
     } catch (err) {
